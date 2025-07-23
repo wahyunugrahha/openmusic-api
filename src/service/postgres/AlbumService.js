d9cc5832ae21737cd73fab69709bf14ef2531a67
wahyunugrahha <wahyunih297@gmail.com>
2025-07-23 17:27:51 +0700

    Fix Error

--- src/service/postgres/AlbumService.js
+++ src/service/postgres/AlbumService.js
@@ -10,8 +10,8 @@ class AlbumService {
     const id = nanoid(16);
 
     const query = {
-      text: 'SELECT * FROM albums WHERE id = $1',
-      values: id,
+      text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
+      values: [id, name, year],
     };
 
     const result = await this._pool.query(query);
--- src/validator/album/album-schema.js
+++ src/validator/album/album-schema.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
 
 const createAlbumValidation = Joi.object({
-  id: Joi.number().min(1).positive().required(),
-  title: Joi.string().required(),
+  name: Joi.string().required(),
   year: Joi.number().positive().required(),
 });
+
+module.exports = { createAlbumValidation };
